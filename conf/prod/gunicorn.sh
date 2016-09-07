db0b5376014a544a41e5cc753d194aeede138cf5
Javier Machin Matos <Javier Machin Matos>
2016-09-07 18:05:21 -0300

    Update user:config deploy

--- conf/prod/gunicorn.sh
+++ conf/prod/gunicorn.sh
@@ -3,17 +3,17 @@ set -e
 APP_NAME=Gosocket_API
 
 # user/group to run as
-USER=ubuntu
-GROUP=ubuntu
+USER=azureuser
+GROUP=azureuser
 
 LOGFILE=/home/$USER/logs/$APP_NAME/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 
-cd /home/ubuntu/src/Gosocket_API
-source /home/ubuntu/venvs/Gosocket_API/bin/activate
+cd /home/$USER/src/Gosocket_API
+source /home/$USER/venvs/Gosocket_API/bin/activate
 
 test -d $LOGDIR || mkdir -p $LOGDIR
 
-exec /home/ubuntu/venvs/Gosocket_API/bin/gunicorn -c /home/ubuntu/src/Gosocket_API/conf/prod/gunicorn.conf \
+exec /home/$USER/venvs/Gosocket_API/bin/gunicorn -c /home/$USER/src/Gosocket_API/conf/prod/gunicorn.conf \
 --user=$USER --group=$GROUP --log-level=debug \
 --log-file=$LOGFILE 2>>$LOGFILE Gosocket.wsgi
