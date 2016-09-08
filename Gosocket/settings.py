3465c9dc7e48efa79f75ea0b3db24c29006d5c6c
HAVY\Havy_DCC <havy211@gmail.com>
2016-09-08 11:38:20 -0300

    update: view :settings

--- Gosocket/settings.py
+++ Gosocket/settings.py
@@ -153,6 +153,3 @@ try:
     from local_settings import *
 except ImportError:
     pass
-
-
-
--- regression/views.py
+++ regression/views.py
@@ -1,52 +1,58 @@
 import pickle
-import datetime
+import logging
 import numpy as np
 import pandas as pd
 from django import forms
 
 from Gosocket import settings
 from .forms import EstimateForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Regression_ideal
+
+logger = logging.getLogger(__name__)
 
 
 def get_values(request):
-    # if this is a POST request we need to process the form data
-    context = {"rutPredictions": "12345678-k", "predictions": 0, "date_value": "mm/dd/yy"}
-    if request.method == 'POST':
-        form_view = EstimateForm(request.POST)
+    try:
+        # if this is a POST request we need to process the form data
+        context = {"rutPredictions": "12345678-k", "predictions": 0, "date_value": "mm/dd/yy"}
+        if request.method == 'POST':
+            form_view = EstimateForm(request.POST)
 
-        if form_view.is_valid():
+            if form_view.is_valid():
 
-            receptor_rut = form_view.cleaned_data["receptor_rut"]
-            date_value = form_view.cleaned_data["date_value"]
+                receptor_rut = form_view.cleaned_data["receptor_rut"]
+                date_value = form_view.cleaned_data["date_value"]
 
-            # query = get_object_or_404(Regression_ideal, receptor_rut=receptor_rut)
+                # query = get_object_or_404(Regression_ideal, receptor_rut=receptor_rut)
 
-            try:
-                query = Regression_ideal.objects.get(receptor_rut=receptor_rut)
-                date_new_value = (pd.Timestamp(date_value, tz='UTC') - pd.Timestamp(query.date_min,
-                                                                                    tz='UTC')) / np.timedelta64(1, 'D')
-                with open(settings.MODELS_PATH + receptor_rut + '.pkl', 'rb') as f:
-                    clf = pickle.load(f)
-                    predictions = clf.predict(date_new_value)
+                try:
+                    query = Regression_ideal.objects.get(receptor_rut=receptor_rut)
+                    date_new_value = (pd.Timestamp(date_value, tz='UTC') - pd.Timestamp(query.date_min,
+                                                                                        tz='UTC')) / np.timedelta64(1,
+                                                                                                                    'D')
+                    with open(settings.MODELS_PATH + receptor_rut + '.pkl', 'rb') as f:
+                        clf = pickle.load(f)
+                        predictions = clf.predict(date_new_value)
 
-                    print("Entro al post", date_value, receptor_rut, date_new_value)
+                        print("Entro al post", date_value, receptor_rut, date_new_value)
 
-                context = {"rutPredictions": query.receptor_rut, "predictions": np.round(predictions, 2)[0],
-                           "date_value": date_value,
-                           'form_view': form_view}
-                return render(request, 'estimate.html', context)
+                    context = {"rutPredictions": query.receptor_rut, "predictions": np.round(predictions, 2)[0],
+                               "date_value": date_value,
+                               'form_view': form_view}
+                    return render(request, 'estimate.html', context)
 
-            except Regression_ideal.DoesNotExist:
-                form_view.full_clean()
-                raise forms.ValidationError("You have forgotten about Fred!")
+                except Regression_ideal.DoesNotExist:
+                    form_view.full_clean()
+                    raise forms.ValidationError("You have forgotten about Fred!")
 
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            form_view = EstimateForm()
+            context = {"rutPredictions": "12345678-k", "predictions": 0.00, "date_value": " dd/mm/yy",
+                       'form_view': form_view}
+        return render(request, 'estimate.html', context)
 
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form_view = EstimateForm()
-        context = {"rutPredictions": "12345678-k", "predictions": 0.00, "date_value": " dd/mm/yy",
-                   'form_view': form_view}
-    return render(request, 'estimate.html', context)
+    except Exception as exc:
+        logger.error(exc, exc_info=True)
+        raise exc
